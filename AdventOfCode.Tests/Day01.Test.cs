2f6595b74c7daee52a8d3d2fbc2c1e16c699efc7
Mark Dodson <mark.dodson@spektrix.com>
2023-12-01 17:25:57 +0000

    Day 1 - Part 2

--- AdventOfCode.Tests/Day01.Test.cs
+++ AdventOfCode.Tests/Day01.Test.cs
@@ -2,11 +2,64 @@ namespace AdventOfCode.Tests;
 
 public class Day01
 {
-    private const string CalibrationLocation = @"C:\repos\freedom-friday\AoC_2023\AdventOfCode.Tests\Inputs\01.txt";
+    private const string Part1Input = @"C:\repos\freedom-friday\AoC_2023\AdventOfCode.Tests\Inputs\01_1.txt";
+    private const string Part2Input = @"C:\repos\freedom-friday\AoC_2023\AdventOfCode.Tests\Inputs\01_2.txt";
+
 
     [Test]
     public void ShouldGetCalibrationSum()
     {
-        Assert.That(AdventOfCode.Day01.GetSumValue(File.ReadAllText(CalibrationLocation)), Is.EqualTo(142));
+        Assert.That(AdventOfCode.Day01.GetSumValue(File.ReadAllText(Part1Input)), Is.EqualTo(142));
+    }
+
+    [Test]
+    public void ShouldGetWordyCalibrationSum()
+    {
+        Assert.That(AdventOfCode.Day01.GetWordySumValue(File.ReadAllText(Part2Input)), Is.EqualTo(281));
+    }
+
+    [TestCase("two1nine", 2)]
+    [TestCase("eightwothree", 8)]
+    [TestCase("abcone2threexyz", 1)]
+    [TestCase("xtwone3four", 2)]
+    [TestCase("4nineeightseven2", 4)]
+    [TestCase("zoneight234", 1)]
+    [TestCase("7pqrstsixteen", 7)]
+    [TestCase("eighthree", 8)]
+    [TestCase("sevenine", 7)]
+    [TestCase("threehpbsevenffnqgdjcftjkdjhhk7dvzmkmqthreefflb", 3)]
+    public void ShouldGetFirstWordedNumber(string input, int expected)
+    {
+        Assert.That(AdventOfCode.Day01.GetFirstWordyNumber(input), Is.EqualTo(expected));
+    }
+
+    [TestCase("two1nine", 9)]
+    [TestCase("eightwothree", 3)]
+    [TestCase("abcone2threexyz", 3)]
+    [TestCase("xtwone3four", 4)]
+    [TestCase("4nineeightseven2", 2)]
+    [TestCase("zoneight234", 4)]
+    [TestCase("7pqrstsixteen", 6)]
+    [TestCase("eighthree", 3)]
+    [TestCase("sevenine", 9)]
+    [TestCase("threehpbsevenffnqgdjcftjkdjhhk7dvzmkmqthreefflb", 3)]
+    public void ShouldGetLastWordedNumber(string input, int expected)
+    {
+        Assert.That(AdventOfCode.Day01.GetLastWordyNumber(input), Is.EqualTo(expected));
+    }
+
+    [TestCase("two1nine", 29)]
+    [TestCase("eightwothree", 83)]
+    [TestCase("abcone2threexyz", 13)]
+    [TestCase("xtwone3four", 24)]
+    [TestCase("4nineeightseven2", 42)]
+    [TestCase("zoneight234", 14)]
+    [TestCase("7pqrstsixteen", 76)]
+    [TestCase("eighthree", 83)]
+    [TestCase("sevenine", 79)]
+    [TestCase("threehpbsevenffnqgdjcftjkdjhhk7dvzmkmqthreefflb", 33)]
+    public void ShouldGetWordySumValue(string input, int expected)
+    {
+        Assert.That(AdventOfCode.Day01.GetWordyPairedNumber(input), Is.EqualTo(expected));
     }
 }
--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,23 +1,39 @@
 ﻿namespace AdventOfCode;
 
-public class Day01 : BaseDay
+public sealed class Day01 : BaseDay
 {
-    private readonly string _input;
+    private static readonly string[] WrittenNumbers =
+        { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+    private static readonly Dictionary<string, int> WordToNumberLookup = new()
+    {
+        { "one", 1 }, { "two", 2 }, { "three", 3 }, { "four", 4 }, { "five", 5 }, { "six", 6 }, { "seven", 7 },
+        { "eight", 8 }, { "nine", 9 }
+    };
+
     private readonly int _part1Solution;
+    private readonly int _part2Solution;
+
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        var input = File.ReadAllText(InputFilePath);
         Console.WriteLine("Hello, World!");
         Console.WriteLine(InputFilePath);
 
-        _part1Solution = GetSumValue(_input);
-
+        _part1Solution = GetSumValue(input);
+        _part2Solution = GetWordySumValue(input);
     }
 
-    public override ValueTask<string> Solve_1() => new($"{_part1Solution}");
+    public override ValueTask<string> Solve_1()
+    {
+        return new ValueTask<string>($"{_part1Solution}");
+    }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2()
+    {
+        return new ValueTask<string>($"{_part2Solution}");
+    }
 
     private static int GetFirstNumber(string calibrationInput)
     {
@@ -50,7 +66,7 @@ public class Day01 : BaseDay
         return Convert.ToInt32("" + GetFirstNumber(input) + GetLastNumber(input));
     }
 
-    private static IEnumerable<string> SplitDocument(string inputDocument)
+    private static string[] SplitDocument(string inputDocument)
     {
         var splitDocument = inputDocument.Split("\r\n");
 
@@ -64,8 +80,61 @@ public class Day01 : BaseDay
         return splitDocument.Sum(GetPairedNumber);
     }
 
-    private static string GetCalibrationFile(string calibrationLocation)
+    public static int GetWordyPairedNumber(string inputFile)
     {
-        return File.ReadAllText(calibrationLocation);
+        return Convert.ToInt32("" + GetFirstWordyNumber(inputFile) + GetLastWordyNumber(inputFile));
+    }
+
+    public static int GetWordySumValue(string inputFile)
+    {
+        var splitDocument = SplitDocument(inputFile);
+
+        return splitDocument.Sum(GetWordyPairedNumber);
+    }
+
+    public static int GetFirstWordyNumber(string inputFile)
+    {
+        var firstRealNumber = GetFirstNumber(inputFile);
+        var firstRealNumberIndex = inputFile.IndexOf(firstRealNumber.ToString(), StringComparison.Ordinal);
+
+        var earliestWord = firstRealNumber;
+        var earliestIndex = firstRealNumberIndex;
+
+        foreach (var writtenNumber in WrittenNumbers)
+        {
+            var indexOf = inputFile.IndexOf(writtenNumber, StringComparison.Ordinal);
+
+            if ((indexOf < earliestIndex && indexOf != -1) || earliestIndex == -1)
+            {
+                earliestIndex = indexOf;
+
+                WordToNumberLookup.TryGetValue(writtenNumber, out earliestWord);
+            }
+        }
+
+        return earliestWord;
+    }
+
+    public static int GetLastWordyNumber(string inputFile)
+    {
+        var lastRealNumber = GetLastNumber(inputFile);
+        var lastRealNumberIndex = inputFile.LastIndexOf(lastRealNumber.ToString(), StringComparison.Ordinal);
+
+        var lastWord = lastRealNumber;
+        var lastIndex = lastRealNumberIndex;
+
+        foreach (var writtenNumber in WrittenNumbers)
+        {
+            var indexOf = inputFile.LastIndexOf(writtenNumber, StringComparison.Ordinal);
+
+            if ((indexOf > lastIndex && indexOf != -1) || lastIndex == -1)
+            {
+                lastIndex = indexOf;
+
+                WordToNumberLookup.TryGetValue(writtenNumber, out lastWord);
+            }
+        }
+
+        return lastWord;
     }
 }
